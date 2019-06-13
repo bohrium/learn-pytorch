4b46b2cc8da537cd18bcbd07e2af2d7388d3e1eb
Samuel Tenka <stenka@fb.com>
2019-06-13 11:45:14 -0700

    learn mnist

--- mnist.py
+++ mnist.py
@@ -19,16 +19,14 @@ class SimpleNet(nn.Module):
         super(SimpleNet, self).__init__()
         self.conv1 = nn.Conv2d(in_channels= 1, out_channels=16, kernel_size=5, stride=2)
         self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=2)
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1)
-        self.fc1 = nn.Linear(2*2*64, 256)
+        self.fc1 = nn.Linear(4*4*32, 256)
         self.fc2 = nn.Linear(256, 10)
 
     def forward(self, x):
                                     # batch x  1 x 28 x 28
         x = F.relu(self.conv1(x))   # batch x 16 x 12 x 12
         x = F.relu(self.conv2(x))   # batch x 32 x  4 x  4
-        x = F.relu(self.conv3(x))   # batch x 64 x  2 x  2
-        x = x.view(-1, 2*2*64)
+        x = x.view(-1, 4*4*32)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
